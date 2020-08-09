b5cbba3ff77d9e19671585c9aa1bcfa87a77cf27
fatih ustunyer <fustunyer1@gmail.com>
2020-08-09 23:11:50 +0300

    ikinci satir ekledim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -1,5 +1,9 @@
 package git_project_ikinci;
 
 public class Deneme {
+	public static void main(String[] args) {
+		System.out.println("ikinci projem");
+		System.out.println("ikinci satir kodum");
+	}
 
 }
