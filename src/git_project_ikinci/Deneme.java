d3222cb30b0560f3d73618ecbba589884ddd8826
fustunyer <67637972+fustunyer@users.noreply.github.com>
2020-08-09 23:20:11 +0300

    Update Deneme.java
    github da degisiklik yaptım.

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -4,6 +4,8 @@ public class Deneme {
 	public static void main(String[] args) {
 		System.out.println("ikinci projem");
 		System.out.println("ikinci satir kodum");
+		System.out.println("remote degisiklik");
+		
 	}
 
 }
